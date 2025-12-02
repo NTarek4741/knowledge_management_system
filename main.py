ab93665ec4591b1381eef274ea41c29802a4d339
Tarek Noiem <tarek_noiem@icloud.com>
2025-12-02 17:52:29 -0500

    FastAPI Init

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from knowledge-management-system!")
+import fastapi
 
+app = fastapi.FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
